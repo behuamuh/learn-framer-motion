208b56037266005c3e8e92ea97ac9728d66fe0fe
Behuamuh <sven1980@list.ru>
2021-01-11 21:53:16 +0300

    init done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import { Card, CardGrid, Container, Header } from './styles/primitives';
 import './App.css';
+import blue from './assets/images/blue.png';
+import purp from './assets/images/purp.png';
+import black from './assets/images/black.png';
+import green from './assets/images/green.png';
+import { MenuIcon } from './assets/icons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>
+        <MenuIcon />
+        <h1>Header</h1>
+      </Header>
+      <Container>
+        <h2>Super Cool</h2>
+        <CardGrid>
+          <Card style={{ background: 'var(--purp)' }}>
+            <h3>Some card</h3>
+            <img src={purp} alt="Card" />
+          </Card>
+          <Card style={{ background: 'var(--blue)' }}>
+            <h3>Some card</h3>
+            <img src={blue} alt="Card" />
+          </Card>
+          <Card style={{ background: 'var(--black)' }}>
+            <h3>Some card</h3>
+            <img src={black} alt="Card" />
+          </Card>
+          <Card style={{ background: 'var(--green)' }}>
+            <h3>Some card</h3>
+            <img src={green} alt="Card" />
+          </Card>
+        </CardGrid>
+      </Container>
     </div>
   );
 }
